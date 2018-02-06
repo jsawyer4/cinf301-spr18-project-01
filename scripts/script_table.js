339f709d8ec4836641f7f103c45c6d24e30fcbda
John Sawyer <jsawyer@stetson.edu>
2018-02-06 17:36:46 -0500

    Got the only blank spaces move part done

--- scripts/script_table.js
+++ scripts/script_table.js
@@ -7,37 +7,82 @@ window.onload = function() {
     const rows = document.querySelectorAll('tr');
     const rowsArray = Array.from(rows);
     const tiles = document.querySelectorAll('td');
-    const tilesArray= Array.from(tiles);
-    var solution = ["<td>1</td>","<td>2</td>","<td>3</td>","<td>4</td>","<td>5</td>","<td>6</td>","<td>7</td>","<td>9</td>","<td>8</td>"];
+    // const tilesSolution = starting_elemts(tiles);
 
     table.addEventListener('click', (event) => {
+
         const rowIndex = rowsArray.findIndex(row => row.contains(event.target));
     const columns = Array.from(rowsArray[rowIndex].querySelectorAll('td'));
     const columnIndex = columns.findIndex(column => column == event.target);
     console.log(rowIndex, columnIndex);
-    console.log(tilesArray[rowIndex*columnIndex]);
-    console.log(solution[rowIndex*columnIndex]);
-    switch_elems(rowIndex, columnIndex);
-
-    if(tilesArray==solution) {
-        alert("You won");
+    var blank;
+    for(i=0;i<tiles.length;i++)
+    {
+        if(tiles[i].innerHTML==' ')
+        {
+            blank=i+1;
+        }
     }
+    if(rowIndex==1)
+    {
+        console.log(tiles[(rowIndex+columnIndex)-1].innerHTML)
+        O=(rowIndex+columnIndex);
+    }
+    else if(rowIndex==2)
+    {
+        console.log(tiles[(rowIndex+(2+columnIndex))-1].innerHTML);
+        O=(rowIndex+(2+columnIndex));
+    }
+    else
+    {
+        console.log(tiles[(rowIndex+(4+columnIndex))-1].innerHTML);
+        O=(rowIndex+(4+columnIndex));
+    }
+    console.log("O",O,"blank",blank);
+    if((blank+3)==O)
+    {
+        switch_elems(rowIndex, columnIndex,(rowIndex-1), columnIndex);
+        console.log("+3");
+    }
+    else if((blank-3)==O)
+    {
+        switch_elems(rowIndex, columnIndex,(rowIndex+1),columnIndex);
+        console.log("-3");
+    }
+    else if((blank-1)==O)
+    {
+        switch_elems(rowIndex, columnIndex,rowIndex,(columnIndex+1));
+        console.log("-1");
+    }
+    else if((blank+1)==O)
+    {
+        switch_elems(rowIndex, columnIndex,rowIndex,(columnIndex-1));
+        console.log("+1");
+    }
+    else
+    {
+
+    }
+
+
 })
 }
-
-function switch_elems(i, j) {
+function switch_elems(i, j,k,l) {
     const table = document.querySelector('table');
     const val1 = table.rows[i].cells[j].innerHTML;
-
+    console.log(i,j,k,l)
+/*
     let k = j + 1;
     let numRows = table.rows.length; // not used, but this gets num rows
     if (k > table.rows[i].cells.length - 1) {
         k = 0;
     }
-    const val2 = table.rows[i].cells[k].innerHTML;
+*/
+
+    const val2 = table.rows[k].cells[l].innerHTML;
 
     table.rows[i].cells[j].innerHTML = val2.toString();
-    table.rows[i].cells[k].innerHTML = val1.toString();
+    table.rows[k].cells[l].innerHTML = val1.toString();
 }
 
 /*
